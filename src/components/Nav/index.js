c4fbdbf3f2ab5b0cf605c323689599de5bfdfca8
Susie <45341415+Haldevel@users.noreply.github.com>
2019-04-24 20:37:44 -0700

    correct wrapper layout

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -4,9 +4,9 @@ import React from "react";
 function Nav(props) {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark" style={{backgroundColor: "#0e8ff8"}}>
-      <a className="navbar-brand" href="/" style={{fontSize: '1.5rem'}}>{props.result}
+      <a className="navbar-brand" href="/" style={{fontSize: '2rem'}}>{props.result}
       </a>
-      <div className="navbar-brand ml-auto" style={{fontSize: '1.5rem'}}>Score: {props.score} | Top Socore: {props.topscore}</div>
+      <div className="navbar-brand ml-auto" style={{fontSize: '2rem'}}>Score: {props.score} | Top Socore: {props.topscore}</div>
     </nav>
   );
 }
--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 //Wrapper component will contain a list of MemoryCard components
 function Wrapper(props) {
-  return <div className="wrapper">{props.children}</div>;
+  return <div className="wrapper" style={{width: '80%'}}>{props.children}</div>;
 }
 
 export default Wrapper;
