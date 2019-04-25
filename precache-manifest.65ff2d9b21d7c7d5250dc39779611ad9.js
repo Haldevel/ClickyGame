fd43fea5373319905b97ad93aeb1745bd659e13b
Susie <45341415+Haldevel@users.noreply.github.com>
2019-04-24 21:31:18 -0700

    Updates

---
+++ precache-manifest.65ff2d9b21d7c7d5250dc39779611ad9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e97f52afa6b92488dce798ccea62f32",
+    "url": "/ClickyGame/index.html"
+  },
+  {
+    "revision": "1c27d5d3e5e8df91313e",
+    "url": "/ClickyGame/static/css/main.b7b4b78e.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/ClickyGame/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "1c27d5d3e5e8df91313e",
+    "url": "/ClickyGame/static/js/main.66874ec8.chunk.js"
+  },
+  {
+    "revision": "95fffdb9eb36913c2817",
+    "url": "/ClickyGame/static/js/runtime~main.1eb9bb0a.js"
+  }
+]);
